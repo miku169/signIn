ed357e7844f3f5d2f9c82faa66a00f244b4df2fb
linhongping <50254968+ccreatfuture1@users.noreply.github.com>
2022-04-01 19:43:33 +0800

    Update aotoSignIn.py

--- aotoSignIn.py
+++ aotoSignIn.py
@@ -1,108 +1,138 @@
-! usr/bin/python
+#! usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
+import requests
 import time
+import re
+import urllib
+import os
 from datetime import datetime
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
-import requests as re
+import random
 
 # 系统变量
-StuID = os.environ['STUID']
-PW = os.environ['PW']
-SERVER = os.environ['SERVER']
-SCKEY = os.environ['SCKEY']
-MAIL_NOTICE = os.environ['MAIL_NOTICE']
-MAILBOX = os.environ['MAILBOX']
+card_id = "1161949599871569922"
+sfzMd5 = "ed39281c8b62d37918c6e4efba261b1f"
+school_no = "4136010406"  # 学校代码
+SERVER = "on"
+SCKEY = "SCT135282T3xQT7veStDDDfSdnliLnGt3W"
 
-CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
-
-# 以下内容无需修改
-mail_host = 'smtp.qq.com'
-mail_sender = '973006245@qq.com'
-mail_pw = 'ihoezpquhuawbfif'
-url = 'https://xmuxg.xmu.edu.cn/app/214'
 dkStart = datetime.now()
+def ali_pay_login():
+    url = f"https://fxgl.jx.edu.cn/{school_no}/third/alipayLogin?cardId={card_id}&sfzMd5={sfzMd5}"
+    accept_encoding = "gzip, deflate, br"
+    accept_language = "zh-CN,en-US;q=0.8"
+    connection = "keep-alive"
+    host = "fxgl.jx.edu.cn"
+    login = requests.get(url, headers={
+        # 'Accept': accept,
+        'Accept-Encoding': accept_encoding,
+        'Accept-Language': accept_language,
+        'Connection': connection,
+        # 'Cookie': cookie,
+        'Host': host,
+        # 'User-Agent': user_agent,
+        # 'sign': sign,
+        # 'ts': ts
+    })
+    return login.history[0]  # 此处重定向到新的地址
+
+
+def get_cookies(value: "ali_pay_login返回的对象"):
+    cookie_str = value.headers["Set-Cookie"]
+    cookies = {}
+    jseSion = (re.search('JSESSIONID=(.+?);', cookie_str)).group(0).replace(';', '')
+    loginName = (re.search('loginName=(.+?);', cookie_str)).group(0).replace(';', '')
+    loginType = (re.search('loginType=(.+?);', cookie_str)).group(0).replace(';', '')
+    yzxx = (re.search('yzxx=(.+?);', cookie_str)).group(0).replace(';', '')
+    for e in [jseSion, loginName, loginType, yzxx]:
+        # print(e)
+        name, val = e.split('=', 1)
+        cookies[name] = val
+    return cookies
+
+
+def checkin(my_cookies):
+    url = f"https://fxgl.jx.edu.cn/{school_no}/studentQd/saveStu"
+    accept = '*/*'
+    accept_encoding = 'gzip, deflate, br'
+    accept_language = 'zh-CN,en-US;q=0.8'
+    connection = 'keep-alive'
+    content_type = 'application/x-www-form-urlencoded; charset=UTF-8'
+    host = 'fxgl.jx.edu.cn'
+    user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) ' \
+                 'Mobile/18E199 Ariver/1.1.0 AliApp(AP/10.2.33.6200) Nebula WK RVKType(1) AlipayDefined(nt:4G,' \
+                 'ws:390|780|3.0) AlipayClient/10.2.33.6200 Language/zh-Hans Region/CN MiniProgram APXWebView ' \
+                 'NebulaX/1.0.0X-Requested-With: XMLHttpRequest '
+    data = {'province': '江西省',
+            'city': '南昌市',
+            'district': '红谷滩区',
+            'street': '丰和南大道696号南昌航空大学',
+            'xszt': 0,
+            'jkzk': 0,
+            'jkzkxq': '',
+            'sfgl': 1,
+            'gldd': '',
+            'mqtw': 0,
+            'mqtwxq': '',
+            'zddlwz': '江西省南昌市红谷滩区丰和南大道696号南昌航空大学',
+            'sddlwz': '',
+            'bprovince': '江西省',
+            'bcity': '南昌市',
+            'bdistrict': '红谷滩区',
+            'bstreet': '丰和南大道696号南昌航空大学',
+            'sprovince': '江西省',
+            'scity': '南昌市',
+            'sdistrict': '红谷滩区',
+            'lng': '115.826517',
+            'lat': '28.649543',
+            'sfby': 1
+
+            }
+    data = urllib.parse.urlencode(data)
+    res = requests.post(url, headers={
+        'Accept': accept,
+        'Accept-Encoding': accept_encoding,
+        'Accept-Language': accept_language,
+        'Connection': connection,
+        'Content-Type': content_type,
+        'Host': host,
+        'User-Agent': user_agent
+    }, cookies=my_cookies, data=data)
+    # print(res.text)
+    return res.text
+
+
+def print_log():
+    try:
+        f = open('lhplog.json', 'r', encoding='utf-8')
+        log = f.read()
+        print(log)
+        f.close()
+    except IOError:
+        print('cant open lhplog.json!')
+
+def create_log(text):
+    msg = (re.search(r'("msg":.*?),', text)).group(1)
+    tm = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    l = tm + ',' + msg
+    return l
+
 
 # 自动打卡
 def autoSignIn():
     global dkStart
+    time.sleep(int(random.random() * 1000 // 1 % 60))
     try:
-        dkStart = datetime.now()
-        print('正在打卡...')
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
-        # driver = webdriver.Chrome(CHROMEDRIVER_PATH)
-        driver.get(url)
-        driver.maximize_window()
-        driver.delete_all_cookies()
+        location = ali_pay_login()
+        myCookies = get_cookies(location)
+        t = checkin(myCookies)
+        log = create_log(t)
+        sendMsg(log)
 
-        time.sleep(3)
-        driver.find_element_by_xpath('//*[@id="loginLayout"]/div[3]/div[2]/div/button[2]').click()
-        time.sleep(1)
-
-        print('正在登录...')
-        driver.find_element_by_xpath('//input[@id="username"]').send_keys(StuID)
-        driver.find_element_by_xpath('//input[@id="password"]').send_keys(PW)
-        driver.find_element_by_xpath('//input[@id="password"]').send_keys(Keys.ENTER)
-        time.sleep(1)
-        print('登录成功...\n正在更改表单...')
-
-        # 判断学号密码是否正确
-        title = str(driver.title)
-        if '厦门大学' in title:
-            print('登录成功...\n正在更改表单...')
-        elif title == '统一身份认证' or title == 'Unified Identity Authentication':
-            msg = '学号或密码错误？or 登录频繁，需要验证码登录？'
-            print('Error: {}'.format(msg))
-            sendMsg(msg)
-            return 0
-        else:
-            pass
-
-        # 点击daily health report
-        driver.find_element_by_xpath('//*[@id="mainPage-page"]/div[1]/div[3]/div[2]/div[2]/div[3]/div/div[1]/div[2]').click()
-        time.sleep(1)
-
-        window1 = driver.current_window_handle
-        for handle in driver.window_handles:
-            if handle != window1:
-                driver.switch_to.window(handle)
-                break
-
-        time.sleep(1)
-
-        # 点击我的表单
-        driver.find_element_by_xpath('//*[@id="mainM"]/div/div/div/div[1]/div[2]/div/div[3]/div[2]').click()
-        time.sleep(2)
-
-        span = driver.find_element_by_xpath('//*[@id="select_1582538939790"]/div/div/span[1]')
-        # 判断是否已打卡！
-        if span.get_attribute('innerHTML') == '是 Yes':
-            print("今日已打卡！请勿重复打卡。")
-            driver.quit()
-            sendMsg("今日已打卡！请勿重复打卡。")
-        else:
-            driver.find_element_by_xpath('//*[@id="select_1582538939790"]/div').click()
-            driver.find_element_by_xpath('//span[text()="是 Yes"]').click()
-            time.sleep(1)
-            driver.find_element_by_xpath('//*[@id="pdfDomContent"]/../span/span').click()
-            time.sleep(1)
-            driver.switch_to.alert.accept()
-            time.sleep(2)
-            driver.quit()
-            print('打卡成功！')
-            sendMail("打卡成功！")
-            sendMsg("打卡成功！")
     except Exception as e:
         print('打卡失败！\n{}'.format(str(e)))
         sendMsg("打卡失败！", str(e))
-        sendMail("打卡失败！", str(e))
+
 
 # 发送微信推送消息
 def sendMsg(m, error=''):
@@ -110,33 +140,12 @@ def sendMsg(m, error=''):
         timeNow = time.strftime('%Y-%m-%d', time.localtime())
         duration = datetime.now() - dkStart
         if error == '':
-            msg = '{} {}! 本次打卡耗时{}秒。'.format(timeNow, m, duration.seconds)
+            msg = '{}本次打卡耗时{}秒。'.format(m, duration.seconds)
         else:
             msg = '{} {}!'.format(timeNow, error)
         url = 'https://sc.ftqq.com/{}.send?text={}&desp={}'.format(SCKEY, msg, '{}\n{}'.format(msg, error))
-        re.get(url)
+        requests.get(url)
 
-# 发送邮件通知
-def sendMail(text="健康打卡成功", error=''):
-    print('发送邮件...')
-    if MAIL_NOTICE == 'on':
-        timeNow = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        duration = datetime.now() - dkStart
-        content = "{}\n{}\n本次耗时{}秒！".format(timeNow, text, duration)
-        msg = MIMEText(content, 'plain', 'utf-8')
-        msg["From"] = Header(mail_sender, 'utf-8')
-        msg["To"] = Header(MAILBOX, 'utf-8')
-        subject = "{0}-{1}".format(time.strftime("%Y%m%d", time.localtime()), text)
-        msg["Subject"] = Header(subject, 'utf-8')
-        try:
-            server = smtplib.SMTP()
-            server.connect(mail_host, 25)
-            server.login(mail_sender, mail_pw)
-            server.sendmail(mail_sender, MAILBOX, msg.as_string())
-            server.quit()
-            print("邮件发送成功！")
-        except Exception as e:
-            print("邮件发送失败！\n{}".format(e))
 
 if __name__ == '__main__':
     autoSignIn()
