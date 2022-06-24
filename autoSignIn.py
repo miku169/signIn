2eb2d03d2559a95feab4be591383159f22dc58c7
linhongping <50254968+ccreatfuture1@users.noreply.github.com>
2022-06-24 19:39:54 +0800

    Update autoSignIn.py

--- autoSignIn.py
+++ autoSignIn.py
@@ -1,143 +1,26 @@
-#! usr/bin/python
-# -*- coding: utf-8 -*-
-
+import json
 import requests
 import time
-import re
-import urllib
-import random
+from datetime import datetime
 import pytz
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-from datetime import datetime
-import os
 
 
-tz = pytz.timezone('Asia/Shanghai')
-# 系统变量
-card_id = os.environ['card_id']
-sfzMd5 = os.environ['sfzMd5']
-school_no = os.environ['school_no']  # 学校代码
-SERVER = "on"     #on/off 是否开始sever通知
-SCKEY = os.environ['SCKEY']
+tz = pytz.timezone('Asia/Shanghai')#设置时区
+SERVER = "on"
+SCKEY = "SCT135282T3xQT7veStDDDfSdnliLnGt3W"
 MAIL_NOTICE = 'on'
 MAILBOX = 'lin2472612203@163.com'
 mail_host = 'smtp.qq.com'
 mail_sender = '2472612203@qq.com'
-mail_pw = os.environ['mail_pw']
+mail_pw = "trhirrozwjkddicg"
 
 dkStart = datetime.now()
-def ali_pay_login():
-    url = f"https://fxgl.jx.edu.cn/{school_no}/third/alipayLogin?cardId={card_id}&sfzMd5={sfzMd5}"
-    accept_encoding = "gzip, deflate, br"
-    accept_language = "zh-CN,en-US;q=0.8"
-    connection = "keep-alive"
-    host = "fxgl.jx.edu.cn"
-    login = requests.get(url, headers={
-        # 'Accept': accept,
-        'Accept-Encoding': accept_encoding,
-        'Accept-Language': accept_language,
-        'Connection': connection,
-        # 'Cookie': cookie,
-        'Host': host,
-        # 'User-Agent': user_agent,
-        # 'sign': sign,
-        # 'ts': ts
-    })
-    return login.history[0]  # 此处重定向到新的地址
-
-
-def get_cookies(value: "ali_pay_login返回的对象"):
-    cookie_str = value.headers["Set-Cookie"]
-    cookies = {}
-    jseSion = (re.search('JSESSIONID=(.+?);', cookie_str)).group(0).replace(';', '')
-    loginName = (re.search('loginName=(.+?);', cookie_str)).group(0).replace(';', '')
-    loginType = (re.search('loginType=(.+?);', cookie_str)).group(0).replace(';', '')
-    yzxx = (re.search('yzxx=(.+?);', cookie_str)).group(0).replace(';', '')
-    for e in [jseSion, loginName, loginType, yzxx]:
-        # print(e)
-        name, val = e.split('=', 1)
-        cookies[name] = val
-    return cookies
-
-
-def checkin(my_cookies):
-    url = f"https://fxgl.jx.edu.cn/{school_no}/studentQd/saveStu"
-    accept = '*/*'
-    accept_encoding = 'gzip, deflate, br'
-    accept_language = 'zh-CN,en-US;q=0.8'
-    connection = 'keep-alive'
-    content_type = 'application/x-www-form-urlencoded; charset=UTF-8'
-    host = 'fxgl.jx.edu.cn'
-    user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) ' \
-                 'Mobile/18E199 Ariver/1.1.0 AliApp(AP/10.2.33.6200) Nebula WK RVKType(1) AlipayDefined(nt:4G,' \
-                 'ws:390|780|3.0) AlipayClient/10.2.33.6200 Language/zh-Hans Region/CN MiniProgram APXWebView ' \
-                 'NebulaX/1.0.0X-Requested-With: XMLHttpRequest '
-    data = {'province': '江西省',
-            'city': '南昌市',
-            'district': '红谷滩区',
-            'street': '丰和南大道696号南昌航空大学',
-            'xszt': 0,
-            'jkzk': 0,
-            'jkzkxq': '',
-            'sfgl': 1,
-            'gldd': '',
-            'mqtw': 0,
-            'mqtwxq': '',
-            'zddlwz': '江西省南昌市红谷滩区丰和南大道696号南昌航空大学',
-            'sddlwz': '',
-            'bprovince': '江西省',
-            'bcity': '南昌市',
-            'bdistrict': '红谷滩区',
-            'bstreet': '丰和南大道696号南昌航空大学',
-            'sprovince': '江西省',
-            'scity': '南昌市',
-            'sdistrict': '红谷滩区',
-            'lng': '115.826517',
-            'lat': '28.649543',
-            'sfby': 1
-
-            }
-    data = urllib.parse.urlencode(data)
-    res = requests.post(url, headers={
-        'Accept': accept,
-        'Accept-Encoding': accept_encoding,
-        'Accept-Language': accept_language,
-        'Connection': connection,
-        'Content-Type': content_type,
-        'Host': host,
-        'User-Agent': user_agent
-    }, cookies=my_cookies, data=data)
-    # print(res.text)
-    return res.text
-
-
-
-def create_log(text):
-    msg = (re.search(r'("msg":.*?),', text)).group(1)
-    tm = datetime.now(tz).strftime('%Y-%m-%d-%H:%M:%S')
-    l = tm + ',' + msg
-    return l
-
-
-# 自动打卡
-def autoSignIn():
-    global dkStart
-    try:
-        time.sleep(int(random.random()*1000%60))
-        location = ali_pay_login()
-        myCookies = get_cookies(location)
-        t = checkin(myCookies)
-        log = create_log(t)
-        sendMsg(log)
-        sendMail(log)
-
-    except Exception as e:
-        print('打卡失败！\n{}'.format(str(e)))
-        sendMsg("打卡失败！", str(e))
-        sendMail("打卡失败！", str(e))
-
+stu = ["4136010406","2116085400010","江西省","南昌市","红谷滩区","","115.826517","28.649543"]
+status = {"1001":"打卡成功","1002":"今日已打卡！","1003":"打卡异常"}
+#学生信息
 
 # 发送微信推送消息
 def sendMsg(m, error=''):
@@ -150,17 +33,17 @@ def sendMsg(m, error=''):
             msg = '{} {}!'.format(timeNow, error)
         url = 'https://sc.ftqq.com/{}.send?text={}&desp={}'.format(SCKEY, msg, '{}\n{}'.format(msg, error))
         requests.get(url)
-
+#发送邮件
 def sendMail(text="健康打卡成功", error=''):
     print('发送邮件...')
     if MAIL_NOTICE == 'on':
         timeNow = datetime.now(tz).strftime('%Y-%m-%d-%H:%M:%S')
         duration = datetime.now() - dkStart
-        content = "{}本次耗时{}秒！".format(text, duration.seconds)
+        content = "{}\n{}\n本次耗时{}秒！".format(timeNow, text, duration)
         msg = MIMEText(content, 'plain', 'utf-8')
         msg["From"] = Header(mail_sender, 'utf-8')
         msg["To"] = Header(MAILBOX, 'utf-8')
-        subject = "{0}-{1}".format(time.strftime("%Y%m%d", time.localtime()),"健康打卡")
+        subject = "{0}-{1}".format(time.strftime("%Y%m%d", time.localtime()), text)
         msg["Subject"] = Header(subject, 'utf-8')
         try:
             server = smtplib.SMTP()
@@ -171,5 +54,57 @@ def sendMail(text="健康打卡成功", error=''):
             print("邮件发送成功！")
         except Exception as e:
             print("邮件发送失败！\n{}".format(e))
+def login():
+
+    if stu[0] != '学校代码':
+        # 登录页面，提交学校代码和学号，用于获取cookie，直接get请求
+        loginurl = f'https://fxgl.jx.edu.cn/{stu[0]}/public/homeQd?loginName={stu[1]}&loginType=0'
+        print(loginurl)
+        # 签到页面，需要使用cookie登录，post一系列参数实现签到
+        signinurl = f'https://fxgl.jx.edu.cn/{stu[0]}/studentQd/saveStu'
+        # 请求头
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.68'
+        }
+        # 使用session会话保持技术，可跨请求保留cookie
+        session = requests.session()
+        # 访问登陆界面，获取到用户的cookie，保持于session会话中
+        session.get(loginurl, headers=headers)
+        # 需要post的数据
+        data = {
+            'province': stu[2],  # 省份
+            'city': stu[3],  # 市
+            'district': stu[4],  # 区/县
+            'street': stu[5],  # 具体地址
+            'xszt': 0,
+            'jkzk': 0,  # 健康状况 0:健康 1:异常
+            'jkzkxq': '',  # 异常原因
+            'sfgl': 1,  # 是否隔离 0:隔离 1:未隔离
+            'gldd': '',
+            'mqtw': 0,
+            'mqtwxq': '',
+            'zddlwz': stu[2] + stu[3] + stu[4],  # 省市区
+            'sddlwz': '',
+            'bprovince': stu[2],
+            'bcity': stu[3],
+            'bdistrict': stu[4],
+            'bstreet': stu[5],
+            'sprovince': stu[2],
+            'scity': stu[3],
+            'sdistrict': stu[4],
+            'lng': stu[6],  # 经度
+            'lat': stu[7],  # 纬度
+            'sfby': 1  # 是否为毕业生 0:是 1:否
+        }
+        result = session.post(url=signinurl, data=data, headers=headers).text
+        # 访问接口返回的数据是json字符串，使用loads方法转换为python字典
+        statusCode = json.loads(result)['code']
+        # 根据状态码判断签到状态
+        print(status.get(str(statusCode)))
+        sendMsg(status.get(str(statusCode)))
+        sendMail(status.get(str(statusCode)))
+        print(f"学号为{stu[1]}的同学" + status.get(str(statusCode)))
+
+
 if __name__ == '__main__':
-    autoSignIn()
+    login()
